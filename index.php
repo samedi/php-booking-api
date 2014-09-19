cfc873a7f079fe7e967a3b18d1fc86ff4b7c2830
Michael Siebert <siebertm85@googlemail.com>
2014-09-19 16:15:43 +0200

    fix php variable/const naming

--- index.php
+++ index.php
@@ -4,22 +4,22 @@ spl_autoload_register(function ($class) {
     require 'oauth_2.0_client_php/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 });
 
-const CLIENT_ID = "REPLACEME";
-$CLIENT_SECRET = "REPLACEME";
-$PRACTICE_ID = "REPLACEME";
-$REDIRECT_URI = "http://url-of-this/script.php";
+$client_id = "REPLACEME";
+$client_secret = "REPLACEME";
+$practice_id = "REPLACEME";
+$redirect_uri = "http://REPLACEME/index.php";
 
 
 
-$BASE_URI = "https://patient.samedi.de";
+$base_uri = "https://patient.samedi.de";
 
 // configuration of client credentials
-$client = new OAuth2\Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+$client = new OAuth2\Client($client_id, $client_secret, $redirect_uri);
 
 // configuration of service
 $configuration = new OAuth2\Service\Configuration(
-        $BASE_URI . '/api/auth/v2/authorize',
-        $BASE_URI . '/api/auth/v2/token');
+        $base_uri . '/api/auth/v2/authorize',
+        $base_uri . '/api/auth/v2/token');
 
 // storage class for access token, just implement OAuth2\DataStore interface for
 // your own implementation
@@ -37,15 +37,15 @@ if (isset($_GET['action'])) {
             break;
         case 'userInfo':
             // calls api endpoint with access token
-            $userInfo = json_decode($service->callApiEndpoint($BASE_URI . '/api/booking/v3/user.json'))->user;
+            $userInfo = json_decode($service->callApiEndpoint($base_uri . '/api/booking/v3/user.json'))->user;
             break;
         case 'categories':
             // calls api endpoint with access token
-            $categories = json_decode($service->callApiEndpoint($BASE_URI . "/api/booking/v3/event_categories.json?practice_id=${PRACTICE_ID}&client_id=" . CLIENT_ID))->data;
+            $categories = json_decode($service->callApiEndpoint($base_uri . "/api/booking/v3/event_categories.json?practice_id=${practice_id}&client_id=" . $client_id))->data;
             break;
         case 'eventTypes':
             // calls api endpoint with access token
-            $eventTypes = json_decode($service->callApiEndpoint($BASE_URI . "/api/booking/v3/event_types.json?practice_id=${PRACTICE_ID}&client_id=" . CLIENT_ID . '&event_category_id='.$_GET["category_id"]))->data;
+            $eventTypes = json_decode($service->callApiEndpoint($base_uri . "/api/booking/v3/event_types.json?practice_id=${practice_id}&client_id=" . $client_id . '&event_category_id='.$_GET["category_id"]))->data;
             var_dump($eventTypes);
             break;
         case 'times':
@@ -56,15 +56,15 @@ if (isset($_GET['action'])) {
                 $curDate = time();
             }
 
-            $dates = json_decode($service->callApiEndpoint($BASE_URI . "/api/booking/v3/dates.json?practice_id=${PRACTICE_ID}&client_id=" . CLIENT_ID . '&event_category_id='.$_GET["category_id"] . '&event_type_id='.$_GET["event_type_id"] . '&date=' . date('Y-m-d', $curDate)))->data;
+            $dates = json_decode($service->callApiEndpoint($base_uri . "/api/booking/v3/dates.json?practice_id=${practice_id}&client_id=" . $client_id . '&event_category_id='.$_GET["category_id"] . '&event_type_id='.$_GET["event_type_id"] . '&date=' . date('Y-m-d', $curDate)))->data;
 
 
-            $times = json_decode($service->callApiEndpoint($BASE_URI . "/api/booking/v3/times.json?practice_id=${PRACTICE_ID}&client_id=" . CLIENT_ID . '&event_category_id='.$_GET["category_id"] . '&event_type_id='.$_GET["event_type_id"] . '&date=' . date('Y-m-d', $curDate)))->data;
+            $times = json_decode($service->callApiEndpoint($base_uri . "/api/booking/v3/times.json?practice_id=${practice_id}&client_id=" . $client_id . '&event_category_id='.$_GET["category_id"] . '&event_type_id='.$_GET["event_type_id"] . '&date=' . date('Y-m-d', $curDate)))->data;
             break;
 
         case 'book':
             $result = json_decode($service->callApiEndpoint(
-                $BASE_URI . "/api/booking/v3/book.json?practice_id=${PRACTICE_ID}&client_id=" . CLIENT_ID,
+                $base_uri . "/api/booking/v3/book.json?practice_id=${practice_id}&client_id=" . $client_id,
                 "POST",
                 array(),
                 array(
@@ -102,51 +102,51 @@ $token = $dataStore->retrieveAccessToken();
         <br />
         <a href="?action=userInfo" id="request-api">fetch user info</a><br />
 
-        <? if (isset($userInfo)) { ?>
+        <?php if (isset($userInfo)) { ?>
         User: <?= $userInfo->full_name ?><br/>
         <a href="?action=categories" >fetch categories</a><br />
-        <? } ?>
+        <?php } ?>
 
-        <? if (isset($categories)) { ?>
-            <? foreach ($categories as $category) { ?>
+        <?php if (isset($categories)) { ?>
+            <?php foreach ($categories as $category) { ?>
                 <a href="?action=eventTypes&category_id=<?= $category->id ?>"><?= $category->name ?></a><br/>
-            <? } ?>
-        <? } ?>
+            <?php } ?>
+        <?php } ?>
 
-        <? if (isset($eventTypes)) { ?>
-            <? foreach ($eventTypes as $eventType) { ?>
+        <?php if (isset($eventTypes)) { ?>
+            <?php foreach ($eventTypes as $eventType) { ?>
                 <a href="?action=times&category_id=<?= $_GET['category_id'] ?>&event_type_id=<?= $eventType->id ?>"><?= $eventType->name ?></a><br/>
-            <? } ?>
-        <? } ?>
+            <?php } ?>
+        <?php } ?>
 
-        <? if (isset($dates)) { ?>
+        <?php if (isset($dates)) { ?>
         <h2>Available Dates</h2>
-            <? foreach ($dates as $date) { if ($date->available) {?>
+            <?php foreach ($dates as $date) { if ($date->available) {?>
                 <a href="?action=times&category_id=<?= $_GET['category_id'] ?>&event_type_id=<?= $_GET['event_type_id'] ?>&date=<?= $date->date ?>">
                     <?= $date->date ?>
                 </a><br/>
-            <? } } ?>
+            <?php } } ?>
 
             <h2>Available Times</h2>
 
-            <? foreach ($times as $time) { ?>
+            <?php foreach ($times as $time) { ?>
                 <a href="?action=book&category_id=<?= $_GET['category_id'] ?>&event_type_id=<?= $_GET['event_type_id'] ?>&starts_at=<?= $time->time ?>&token=<?= $time->token ?>">
                     <?= date("H:i", strtotime($time->time)) ?>
                 </a><br/>
-            <? } ?>
+            <?php } ?>
 
-        <? } ?>
+        <?php } ?>
 
-        <? if (isset($result)) { ?>
+        <?php if (isset($result)) { ?>
             <h1>Ergebnis ihrer Terminbuchung</h1>
 
-            <? if (isset($result->error)) { ?>
+            <?php if (isset($result->error)) { ?>
             Fehler: <?= $result->error ?>
-            <? } else { ?>
+            <?php } else { ?>
             Termin gebucht!<br/>
             Termin-Info: <?= $result->data->name ?> <?= $result->data->starts_at ?><br/>
-            <? } ?>
-        <? } ?>
+            <?php } ?>
+        <?php } ?>
 
     </body>
 </html>
